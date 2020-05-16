686029a3c3787f5e22c2a9e1ce7402bbfd13694f
almasmoot <smo15001@byui.edu>
2020-05-15 21:23:04 -0600

    Said Hello to Brennan

--- src/HelloName.java
+++ src/HelloName.java
@@ -1,5 +1,6 @@
 public class HelloName {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brennan");
+        System.out.println("Hello Brennan, My name is Alma");
     }
 }
