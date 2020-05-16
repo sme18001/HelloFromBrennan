56d1fada5c48cc5a422dda029ea8360b409587b6
cortsmeltz <csmeltz2@byui.edu>
2020-05-16 11:55:06 -0600

    Said hello

--- src/HelloName.java
+++ src/HelloName.java
@@ -2,5 +2,6 @@ public class HelloName {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brennan");
         System.out.println("Hello Brennan, My name is Alma");
+        System.out.println("Hello Brennan and Alma, my name is Cort");
     }
 }
